0f2d1658d7061a9c5396605fd556e1439ae19858
tosh7 <zlia.6.lj.425@gmail.com>
2018-10-09 13:52:01 +0900

    first init

---
+++ tosh7/GitHubClientApp/GitHubClientApp/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubClientApp
+//
+//  Created by Satoshi Komatsu on 2018/10/09.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
