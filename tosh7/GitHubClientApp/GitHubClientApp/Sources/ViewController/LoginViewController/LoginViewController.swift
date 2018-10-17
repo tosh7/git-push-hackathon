c81b75e2617767d2b430ccf8b05c1e6e45eb1c70
tosh7 <zlia.6.lj.425@gmail.com>
2018-10-17 10:48:34 +0900

    added APIKit

---
+++ tosh7/GitHubClientApp/GitHubClientApp/Sources/ViewController/LoginViewController/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  GitHubClientApp
+//
+//  Created by Satoshi Komatsu on 2018/10/09.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+import APIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!{
+        didSet {
+            loginButton.isEnabled = false
+        }
+    }
+    
+    let url = "https://api.github.com"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+        let userName = userNameTextField.text
+        let passWord = passWordTextField.text
+        
+        if userName != "" && passWord != "" {
+            //ここでログイン周りの処理を行う
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if userNameTextField.text != "" && passWordTextField.text != ""{
+            loginButton.isEnabled = true
+        }
+        
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if userNameTextField.text != "" && passWordTextField.text != ""{
+            loginButton.isEnabled = true
+        }
+        
+        self.view.endEditing(true)
+    }
+}
+
